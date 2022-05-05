2e4b907b90459be64fa99070fa55de1527e72990
MHajdu <mozes.hajdu@dmgmori-heitec.hu>
2022-05-05 16:03:14 +0200

    initial commit

---
+++ src/main/java/com/mozeshajdu/audiotagcollector/AudioTagCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.mozeshajdu.audiotagcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioTagCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioTagCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mozeshajdu/audiotagcollector/AudioTagCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozeshajdu.audiotagcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTagCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
